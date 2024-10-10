3209673b7cc70fdbc7364079d354672705c64258
00cedke <mathiskera2509@gmail.com>
2024-10-10 18:37:43 +0200

    Updated the config.sh and the README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 # How to use ?
 - Install [Git](https://git-scm.com/downloads), [NodeJS](https://nodejs.org) and [PHP](https://www.php.net/downloads).
 - After that, do `npm install` for install the all packages.
+- And convert the `config.php` to `config.json` with `bash scripts/config.sh config.php`.
 - For start the server do `npm run start` or `bash scripts/server.sh`.
 
 # Credits
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <config.php>"
-  exit 1
+    echo "Usage: config.sh <config.php>"
+    exit 1
 fi
 
-php_file="$1"
+PHP_PATH="/c/php/php.exe"
 
-if [ ! -f "$php_file" ]; then
-  echo "The file $php_file doesn't exist."
-  exit 1
+if [ ! -f "$1" ]; then
+    echo "The PHP File doesn't exist."
+    exit 1
 fi
 
-output_file="${php_file%.php}.json"
-
-php_output=$(php -r "
-include realpath('$php_file');
-echo json_encode(\$data, JSON_PRETTY_PRINT);
-")
-
+OUTPUT=$($PHP_PATH -f "$1" | sed 's/^/{ "config": /; s/$/ }/')
 if [ $? -ne 0 ]; then
-  echo "Error with the PHP File."
-  exit 1
+    echo "Error with the PHP File."
+    exit 1
 fi
 
-echo "$php_output" > "$output_file"
+echo "$OUTPUT" > "${1%.php}.json"
 
-echo "Converted."
+echo "Converted".
